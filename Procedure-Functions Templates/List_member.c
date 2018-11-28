1971fa2d548f142199f1cce8cc292250657cd78b
AlecSadler <45435648+AlecSadler@users.noreply.github.com>
2018-11-28 20:57:39 +0100

    Create List_member.c

---
+++ Procedure-Functions Templates/List_member.c
@@ -0,0 +1,8 @@
+/* returns TRUE if the element "el" is contained in the list */
+
+int member (NodeList head,int el){
+    NodeList cur=head;
+    int ok=0;
+    while (cur != NULL && ok==0){
+        if (cur->value==el){
+            ok=1;}
