c180d1e25863770a49997aa69eeaeb8b087b6df8
AlecSadler <45435648+AlecSadler@users.noreply.github.com>
2018-11-28 21:34:57 +0100

    Create Funzione_Modulo_Differenza.c

---
+++ Lezione6/Funzione_Modulo_Differenza.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void diff_abs (float *x ,float *y){
+    float tmp1=*x;
+    float tmp2=*y;
+    *x=tmp1-tmp2;
+    *y=tmp2-tmp1;
+}
+
+int main(){
+    float a;
+    float b;
+    scanf("%f%f",&a,&b);
+    diff_abs(&a,&b);
+    printf("%.2f\n%.2f\n",a,b);
+    return 0;
+}
