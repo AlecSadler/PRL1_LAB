903074bc528bd8fc96515811d8b8ca2917526ad8
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2019-01-15 23:07:06 +0100

    Update and rename Suffix.ml to Max_Suffix.ml

--- Caml Exercises/Max_Suffix.ml
+++ Caml Exercises/Max_Suffix.ml
@@ -1,7 +1,7 @@
 /* La funzione restituisce la lunghezza del massimo suffisso di una lista di interi strettamente minore di un intero n
 dato come input, senza usare risorsione esplicita */
 
-let suffix n l=
+#let suffix n l=
   let f x (l1,sum)= if sum+x < n then (x::l1,sum+x)
                     else (l1,sum+x)
 in let (a,b)= foldr f ([],0) l in a;;                    
