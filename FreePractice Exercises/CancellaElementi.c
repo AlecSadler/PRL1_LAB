5ccf04e902d62a5fca9e9eca96de00b2fb16038c
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2019-06-28 22:24:51 +0200

    Create CancellaElementi.c

---
+++ FreePractice Exercises/CancellaElementi.c
@@ -0,0 +1,32 @@
+// cancella determinati nodi in base a una condizione (VERSIONE ITERATIVA)
+
+void deleteSomeNode (node **head){
+  node *x=*head;
+  node *prev=NULL;
+  node *fw=x->next;
+  if (*head!=NULL){
+    while (fw!=NULL){
+      if (***CONDITION***){
+        if (prev==NULL){
+          node *tmp=x;
+          *head=(*head)->next;
+          x=*head;
+          fw=x->next;
+          free(tmp);}
+        else{
+          node *tmp=x;
+          prev->next=tmp->next;
+          x=fw;
+          fw=fw->next;
+          free(tmp);}}
+      else{
+        prev=x;
+        x=fw;
+        fw=fw->next;}}
+    if (***CONDITION***){
+      if (prev!=NULL)
+        prev->next=NULL;
+      else
+        *head=NULL;
+      free(x);}}
+}
