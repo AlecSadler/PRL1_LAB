2cf029da012592f95c364109dfa3c8d697b5f1d6
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2019-07-15 11:23:44 +0200

    Update prodottiNegozi.c

--- AppelloLAB 25-06-19/prodottiNegozi.c
+++ AppelloLAB 25-06-19/prodottiNegozi.c
@@ -85,3 +85,37 @@ void main(){
   printf("Prodotti con differenza maggiore di 10 euro: \n" );
   printDiff(list);
 }
+
+// VERSIONE ITERATIVA DELLA FUNZIONE FILTER (CON UNA GENERICA CONDIZIONE DA ADATTARE ALL'ESERCIZIO)
+
+void del3 (node **head){
+  if (*head!=NULL){
+    node *x=*head;
+    node *prev=NULL;
+    node *fw=x->next;
+    while (fw!=NULL){
+      if (x->key>3){
+        if (prev==NULL){
+          node *tmp=x;
+          *head=(*head)->next;
+          x=*head;
+          fw=x->next;
+          free(tmp);}
+        else{
+          node *tmp=x;
+          prev->next=tmp->next;
+          x=fw;
+          fw=fw->next;
+          free(tmp);}}
+      else{
+        prev=x;
+        x=fw;
+        fw=fw->next;}}
+    if (x->key>3){
+      if (prev!=NULL)
+        prev->next=NULL;
+      else
+        *head=NULL;
+      free(x);}}
+}
+
